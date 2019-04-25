14b4e4af01381258858a4336df5fd1830e9b764e
Skipping903 <albieluk000@yahoo.com>
2019-04-25 11:32:21 -0500

    Updated Code
    Updated with working code from class

--- app/src/main/java/com/example/csc300binarytree/MainActivity.java
+++ app/src/main/java/com/example/csc300binarytree/MainActivity.java
@@ -1,22 +1,70 @@
 package com.example.csc300binarytree;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView payloadTV;
+    private Button leftButton, rightButton;
+    private BinaryTree2 myTree;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.payloadTV = this.findViewById(R.id.payloadTV);
+        this.leftButton = this.findViewById(R.id.leftBtn);
+        this.rightButton = this.findViewById(R.id.rightBtn);
 
-        BinaryTree2 bt = new BinaryTree2(5);
-        bt.add(3);
-        bt.add(3);
-        bt.add(8);
-        bt.add(6);
-        bt.visitInOrder();
+        if(this.getIntent().hasExtra("myTree"))
+        {
+            this.myTree = (BinaryTree2) this.getIntent().getSerializableExtra("myTree");
+        }
+        else
+        {
+            BinaryTree2 bt = new BinaryTree2(5);
+            bt.add(3);
+            bt.add(3);
+            bt.add(8);
+            bt.add(6);
+            bt.visitInOrder();
+        }
+
+        this.payloadTV.setText("" + this.myTree.payload);
+        this.hideButtons();
     }
-}
+
+    private void hideButtons()
+    {
+        if(this.myTree.left == null)
+        {
+            this.leftButton.setVisibility(View.INVISIBLE);
+        }
+
+        if(this.myTree.right == null)
+        {
+            this.rightButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void onRightClickButton(View v)
+    {
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("myTree", this.myTree.right);
+        this.startActivity(i);
+    }
+
+    public void onLeftClickButton(View v)
+    {
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("myTree", this.myTree.left);
+        this.startActivity(i);
+    }
+}//class
